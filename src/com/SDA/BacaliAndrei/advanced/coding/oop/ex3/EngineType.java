b9b9a95c5265d9019c330deb20cac65ac5eda3f3
AndreiBacali <bacaliandrei@yahoo.com>
2022-11-26 13:52:57 +0200

    Ex3

---
+++ src/com/SDA/BacaliAndrei/advanced/coding/oop/ex3/EngineType.java
@@ -0,0 +1,14 @@
+package com.SDA.BacaliAndrei.advanced.coding.oop.ex3;
+
+/**
+ * (represented as the enum class, e.g. V12, V8, V6, S6, S4, S3)
+ */
+
+public enum EngineType {
+    V12,
+    V8,
+    V6,
+    S6,
+    S4,
+    S3;
+}
